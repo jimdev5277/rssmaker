41a7fa16cd2eb63914259acac7b000df47fe82c0
jtcdf <bianda132@163.com>
2017-01-04 16:33:34 +0800

    增加初步示例

--- studyfe.js
+++ studyfe.js
@@ -1,7 +1,11 @@
-//引入 `superagent` 库
+//引入superagent库
 const superagent = require('superagent');
 //json 2 xml
-const json2xml = require('json2xml');
+const convert = require('data2xml')({
+    attrProp: '@',
+    valProp: '#',
+    cdataProp: '%'
+});
 // 定时抓取
 const schedule = require('node-schedule');
 // 写入文件操作
@@ -10,32 +14,22 @@ const fs = require('fs');
 superagent.get('https://zhuanlan.zhihu.com/api/columns/study-fe/posts?limit=20')
     .end(function (err, sres) {
         // 常规的错误处理
-        if (err) {
+        if (err || !sres.ok) {
         }
         let content = sres.text;
         content = eval('(' + content + ')');
-        var items = [];
+        let items = [];
+        //拼接RSS的item数组内容
         for (let contenItem of content) {
-            // console.log('图片                     ' + contenItem.content.replace(/src=\\?"(.*?)\.(jpg|png|jpeg|bmp|gif)\\?"/g, 'src="https://pic1.zhimg.com/' + '$1' + '.' + '$2' + '"'));
-            let detailc = '<![CDATA[' + '<img border="none" src="' + contenItem.titleImage + '">' + contenItem.content.replace(/src=\\?"(.*?)\.(jpg|png|jpeg|bmp|gif)\\?"/g, 'src="https://pic1.zhimg.com/' + '$1' + '.' + '$2' + '"') + ']]>';;
+            //拼接封面图
+            let detailc = '<![CDATA[' + '<img border="none" src="' + contenItem.titleImage + '">' + contenItem.content.replace(/src=\\?"(.*?)\.(jpg|png|jpeg|bmp|gif)\\?"/g, 'src="https://pic1.zhimg.com/' + '$1' + '.' + '$2' + '"') + ']]>';
             items.push({
                 title: contenItem.title,
-                link: config.link + '/' +contenItem.href.replace(/\/api\/posts\/(.*)/, '$1') + '?refer=' + config.id,
+                link: 'https://zhuanlan.zhihu.com/study-fe' + '/' + contenItem.url + '?refer=' + 'study-fe',
                 author: contenItem.author.name,
                 pubDate: contenItem.publishedTime,
                 description: detailc
             })
         }
-        // console.log('items的长度'+items.length);
-        for (var itemx = 0; itemx < items.length; itemx++) {
-            var desc = items[itemx].description;
-            delete items[itemx].description;
-            var xmlItems = {
-                item: items[itemx]
-            };
-            desc = '<description>' + desc + '</description>' + '</item>'
-            rssXml = rssXml + json2xml(xmlItems).replace('</item>', '') + desc;
-            // console.log(json2xml(xmlItems));
-        }
-        sendDo();
+        console.info(items);//打印查看数组数据
     });
